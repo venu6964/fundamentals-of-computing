132ae2ff0fe6758a4faa91be20af85fcb18568a2
venu6964 <124232116+venu6964@users.noreply.github.com>
2023-02-05 16:56:53 +0530

    Add files via upload

---
+++ day 2 foc Qu 20.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	long int octal,deci=0;
+	int i=0;
+	printf("enter a octal number:");
+	scanf("%1d",&octal);
+	while(octal !=0)
+	{
+		deci+=(octal % 10)*pow(8, i++);
+		octal/=10;
+	}
+	printf("the decimal value is: %1d",deci);
+	return 0;
+}
