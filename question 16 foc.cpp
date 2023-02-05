c78e942b3c98aba30409cbf797979a62cb39f859
venu6964 <124232116+venu6964@users.noreply.github.com>
2023-02-05 17:02:01 +0530

    Add files via upload

---
+++ question 16 foc.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+	int first,second,temp;
+	printf("enter first  number: ");
+	scanf("%d",&first);
+	printf("enter second number: ");
+	scanf("%d",&second);
+	
+	temp=first;
+	first=second;
+	second=temp;
+	
+	printf("\nAfter swapping, first number=%d\n",first);
+	printf("After swapping, second number=%d",second);
+	return 0;
+}
